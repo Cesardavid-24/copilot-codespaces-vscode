41b216e7c1a7e84c0da28d7ad7b550cee9dd0054
Cesar-david <80113487+Cesardavid-24@users.noreply.github.com>
2023-11-13 14:39:44 +0000

    copilot other commit

--- comments.js
+++ comments.js
@@ -1,24 +1,64 @@
 // create web server
-// create web server
+// 1. create web server
+// 2. create router
+// 3. create router handler
+// 4. listen to port 3000
+
 const express = require('express');
 const app = express();
-const path = require('path');
+const commentsRouter = require('./routes/comments');
 
-// set port
-app.set('port', process.env.PORT || 3000);
+const PORT = 3000;
 
-// set view engine
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-
-// set middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 
-// set routes
-app.use(require('./routes/index'));
+app.use('/comments', commentsRouter);
 
-// start server
-app.listen(app.get('port'), () => {
-    console.log('Server on port', app.get('port'));
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
+
+// Path: routes/comments.js
+// create router
+// 1. create router
+// 2. create router handler
+
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.send('GET /comments');
+});
+
+router.post('/', (req, res) => {
+  res.send('POST /comments');
+});
+
+router.get('/:id', (req, res) => {
+  res.send('GET /comments/:id');
+});
+
+router.patch('/:id', (req, res) => {
+  res.send('PATCH /comments/:id');
+});
+
+router.delete('/:id', (req, res) => {
+  res.send('DELETE /comments/:id');
+});
+
+module.exports = router;
+
+// Path: models/comment.js
+// create model
+// 1. create model
+
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema({
+  text: String,
+});
+
+const Comment = mongoose.model('Comment', commentSchema);  
+
+
